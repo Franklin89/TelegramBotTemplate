29d3bcff59eb86eda4811d25820c02246f658a79
Matteo Locher <matteolocher@gmail.com>
2020-11-24 13:26:59 +0100

    Remove base implementation of chat service commands on the bot

--- src/Commands/IBotCommand.cs
+++ src/Commands/IBotCommand.cs
@@ -10,15 +10,5 @@ namespace TelegramBotTemplate.Commands
         bool InternalCommand { get; }
 
         Task Execute(IChatService chatService, long chatId, int userId, int messageId, string? commandText);
-
-        async Task UnknownCommand(IChatService chatService, long chatId)
-        {
-            await SendMessage(chatService, chatId, Bot.UnknownCommandMessage);
-        }
-
-        async Task SendMessage(IChatService chatService, long chatId, string message, Dictionary<string, string>? buttons = null)
-        {
-            await chatService.SendMessage(chatId, message, buttons);
-        }
     }
 }
